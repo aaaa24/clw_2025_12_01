01d9330b851dc4301b67d688d43c03a9a01ebcb6
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-01 17:01:07 +0300

    fixed some errors

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 namespace top {
   struct p_t {
     int x, y;
-  }
+  };
 
   struct IDraw {
     virtual p_t begin() = 0;
     virtual p_t next(p_t) = 0;
-  }
+  };
 
   bool operator==(p_t a, p_t b)
   {
