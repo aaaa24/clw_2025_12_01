171126c655158b46b3c061ba62bb4c4d2b8c669e
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-08 16:27:37 +0300

    completed Circle::next

--- main.cpp
+++ main.cpp
@@ -210,6 +210,23 @@ top::p_t top::Circle::next(p_t p) const
   if (radius == 1) {
     return begin();
   }
+  const double PI = acos(-1.0);
+  int dx = p.x - o.x;
+  int dy = p.y - o.y;
+  int angle = atan2(dy, dx) * 180.0 / PI;
+  if (angle < 0) {
+    angle += 360;
+  }
+  for (int i = 0; i < 360; ++i) {
+    int new_angle = (angle + i) % 360;
+    int new_x = o.x + radius * cos(new_angle);
+    int new_y = o.y + radius * sin(new_angle);
+    p_t new_p{new_x, new_y};
+    if (new_p != p) {
+      return new_p;
+    }
+  }
+  return p;
 }
 
 void top::make_f(top::IDraw ** b, size_t k)
