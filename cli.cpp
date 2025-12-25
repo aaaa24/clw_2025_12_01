ab3f5fed3cae836ae6e72c0d15f988c3616cbbb2
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-25 15:21:58 +0300

    cli with strs

--- cli.cpp
+++ cli.cpp
@@ -5,18 +5,29 @@ void hi()
   std::cout << "< HI! >\n";
 }
 
+bool is_space(char c)
+{
+  return std::isspace(c);
+}
+
+std::istream & getword(std::istream & is, char * word, size_t k, bool(*c)(char));
+
+size_t match(const char * word, const char * const * words, size_t k);
+
 int main()
 {
   constexpr size_t cmds_count = 1;
   void(*cmds[cmds_count])() = {hi};
+  const char * const cmds_text[] = {"hi"};
 
-  size_t i = 0;
-  while (!(std::cin >> i).eof()) {
+  constexpr size_t bsize = 255;
+  char word[255 + 1] = {};
+  while (!(getword(std::cin, word, bsize, is_space)).eof()) {
     if (std::cin.fail()) {
       std::cerr << "< INVALID COMMAND >\n";
       return 1;
       // Написать пропуск ввода
-    } else if (i < cmds_count) {
+    } else if (size_t i = match(word, cmds_text, cmds_count) < cmds_count) {
       cmds[i]();
     } else {
       std::cerr << "< UNKNOWN COMMAND >\n";
