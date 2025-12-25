fa458676d73f12627920de19d5dcd1a86a6ef40b
aaaa24 <chernov.arseniy@yandex.ru>
2025-12-25 14:59:45 +0300

    completed function main

--- cli.cpp
+++ cli.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
+void hi()
+{
+  std::cout << "< HI! >\n";
+}
+
 int main()
 {
-  
+  size_t i = 0;
+  while (!(std::cin >> i).eof()) {
+    if (std::cin.fail()) {
+      std::cerr << "< INVALID COMMAND >\n";
+      return 1;
+      // Написать пропуск ввода
+    } else if (i == 0) {
+      hi();
+    } else {
+      std::cerr << "< UNKNOWN COMMAND >\n";
+    }
+  }
 }
